1864b70923801b5f8d4284cea8b9b4a66cc89cc8
zohuyhieuzoLC03 <=>
2022-05-13 01:49:19 +0700

    destructor

--- Shiba/main.cpp
+++ Shiba/main.cpp
@@ -11,70 +11,74 @@ using namespace sf;
 */
 
 struct point
-{ int x,y;};
-
-int main()
 {
-    srand(time(0));
-    RenderWindow app(VideoMode(400, 533), "Bonk Game!");
-    app.setFramerateLimit(60);
+    int x,y;
+};
 
-    Texture t1,t2,t3_1,t3_2,t4,t5;
-    t1.loadFromFile("images/background2.jpg");
-    t2.loadFromFile("images/platform.png");
-    t3_1.loadFromFile("images/dog2_left.png");
-    t3_2.loadFromFile("images/dog2_right.png");
-    t4.loadFromFile("images/GameOver_fix.png");
-    t5.loadFromFile("images/bonk_img_resize.jpg");
+const int window_width = 400;
+const int window_height = 533;
+const int background_width = 400;
+const int background_height = 533;
+const int character_width = 80;
+const int character_height = 80;
+const int platform_width = 68;
+const int platform_height = 14;
+const int level[4]= {10,8,6,4};
 
-    Sprite sBackground(t1), sPlat(t2), sPers_left(t3_1), sPers_right(t3_2), sGameOver(t4), sBonk(t5);
-//
-    Font font;
-    font.loadFromFile("font/arial.ttf");
+// Render window
+RenderWindow app(VideoMode(window_width, window_height), "Bonk Game!");
 
-    Text ScoreText;
-    ScoreText.setFont(font);
-    ScoreText.setCharacterSize(20);
-    ScoreText.setFillColor(Color::Red);
+// Texture
+Texture t1,t2,t3_1,t3_2,t4,t5;
 
-    Text Highest_ScoreText;
-    Highest_ScoreText.setFont(font);
-    Highest_ScoreText.setCharacterSize(20);
-    Highest_ScoreText.setFillColor(Color::Red);
-    Highest_ScoreText.setPosition (200,0);
+//Sprite
+Sprite sBackground, sPlat, sPers_left, sPers_right, sGameOver, sBonk;
+// Font
+Font font;
 
-    Text Restart;
-    Restart.setFont(font);
-    Restart.setCharacterSize(30);
-    Restart.setFillColor(Color::Black);
-    Restart.setString("Press F to restart!");
-    Restart.setPosition(80,480);
+//Text
+Text Highest_ScoreText, Restart, ScoreText;
 
-    SoundBuffer buffer;
-    buffer.loadFromFile("sound/jump.wav");
+//Sound
+SoundBuffer buffer,buffer_1;
+Sound jump,bonk;
 
-    Sound jump;
-    jump.setBuffer(buffer);
+//level
+int N = 0;
 
-    SoundBuffer buffer_1;
-    buffer_1.loadFromFile("sound/bonk.wav");
-    Sound bonk;
-    bonk.setBuffer(buffer_1);
+//Score
+int Score=0, Highest_Score=0;
 
-    int Score=0,Highest_Score=0;
-    bool first = true;
-    point plat[20];
-    bool Game = true;
-    for (int i=0;i<10;i++)
-      {
-       plat[i].x=rand()%350;
-       plat[i].y=rand()%533;
-      }
-    plat[0].y=400;
+//Character_position
+int x,y,h;
+
+//Platform_position
+point plat[20];
+
+//Game_condition
+bool Game,first,isRight;
+
+//Velocity
+float dy;
+
+void set_start()
+{
+    Game=true;
+    first=true;
+    for(int i=0; i<level[N]; i++)
+    {
+        plat[i].x=rand()%(background_width-platform_width);
+        plat[i].y=rand()%(background_height);
+    }
+    plat[0].y=300;
     plat[0].x=100;
-    int x=100,y=300,h=200;
-    float dy=0;
-    bool isRight = true;
+    x=100,y=200,h=200;
+    dy=0;
+    Score=0;
+    isRight = true;
+}
+void run()
+{
     while (app.isOpen())
     {
         Event e;
@@ -83,100 +87,141 @@ int main()
             if (e.type == Event::Closed)
                 app.close();
         }
-    if (Game==false)
-    {
-        if(Keyboard::isKeyPressed(Keyboard::F))
+        if (Game==false)
         {
-            Game=true;
-            first=true;
-            for(int i=0;i<10;i++)
+            if(Keyboard::isKeyPressed(Keyboard::F))
             {
-                plat[i].x=rand()%350;
-                plat[i].y=rand()%533;
+                set_start();
             }
-            plat[0].y=400;
-            plat[0].x=100;
-            x=100,y=300,h=200;
-            dy=0;
-            Score=0;
+            continue;
         }
-        continue;
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Right)) {
+        if (Keyboard::isKeyPressed(Keyboard::Right))
+        {
             isRight = true;
             x+=3;
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Left)) {
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Left))
+        {
             isRight = false;
             x-=3;
-    }
-    if (x>400)
-        x=0;
-    if (x<-40)
-        x=400-80;
-    if(y==h&&dy<-2.0)
-    {
-        Score++;
-        ScoreText.setString("Score: " + std::to_string(Score));
-        if(Score>Highest_Score)
-        {
-            Highest_Score=Score;
-            Highest_ScoreText.setString("Highest Score: " + std::to_string(Highest_Score));
         }
-    }
-    dy+=0.2;
-    y+=dy;
-    if (y>500)
-    {
-        Game = false ;
-        app.clear(Color::White);
-        bonk.play();
-        app.draw(sGameOver);
-        sBonk.setPosition(0,200);
-        app.draw(sBonk);
-        app.draw(Restart);
-        app.display();
-        continue;
-    }
-
-    if (y<h)
-    for (int i=0;i<10;i++)
-    {
-      y=h;
-      plat[i].y=plat[i].y-dy;
-      if (plat[i].y>533) {
-            plat[i].y=0;
-            plat[i].x=rand()%350;
+        if (x>background_width)
+            x=0;
+        if (x<-40)
+            x=background_width-character_width;
+        if(y==h&&dy<-2.0)
+        {
+            Score++;
+            ScoreText.setString("Score: " + std::to_string(Score));
+            if(Score>Highest_Score)
+            {
+                Highest_Score=Score;
+                Highest_ScoreText.setString("Highest Score: " + std::to_string(Highest_Score));
             }
-    }
+        }
+        dy+=0.2;
+        y+=dy;
+        if (y>window_height)
+        {
+            Game = false ;
+            app.clear(Color::White);
+            bonk.play();
+            app.draw(sGameOver);
+            sBonk.setPosition(0,200);
+            app.draw(sBonk);
+            app.draw(Restart);
+            app.display();
+            continue;
+        }
 
-    for (int i=0;i<10;i++)
-     if ((x+50>plat[i].x) && (x+20<plat[i].x+68)
-      && (y+70>plat[i].y) && (y+70<plat[i].y+14) && (dy>0)) {
-          dy=-10;
-          jump.play();
-      }
+        if (y<h)
+            for (int i=0; i<level[N]; i++)
+            {
+                y=h;
+                plat[i].y=plat[i].y-dy;
+                if (plat[i].y>background_height)
+                {
+                    plat[i].y=0;
+                    plat[i].x=rand()%(background_width-platform_width);
+                }
+            }
 
-    app.clear(Color::Black);
-    app.draw(sBackground);
-    if(isRight==true)
-    {
-        sPers_right.setPosition(x,y);
-        app.draw(sPers_right);
+        for (int i=0; i<level[N]; i++)
+            if ((x+50>plat[i].x) && (x+20<plat[i].x+68)
+                    && (y+70>plat[i].y) && (y+70<plat[i].y+14) && (dy>0))
+            {
+                dy=-10;
+                jump.play();
+            }
+
+        app.clear(Color::Black);
+        app.draw(sBackground);
+        if(isRight==true)
+        {
+            sPers_right.setPosition(x,y);
+            app.draw(sPers_right);
+        }
+        else
+        {
+            sPers_left.setPosition(x,y);
+            app.draw(sPers_left);
+        }
+        for (int i=0; i<level[N]; i++)
+        {
+            sPlat.setPosition(plat[i].x,plat[i].y);
+            app.draw(sPlat);
+        }
+        app.draw(ScoreText);
+        app.draw(Highest_ScoreText);
+        app.display();
     }
-    else
-    {
-        sPers_left.setPosition(x,y);
-        app.draw(sPers_left);
-    }
-    for (int i=0;i<10;i++)
-    {
-    sPlat.setPosition(plat[i].x,plat[i].y);
-    app.draw(sPlat);
-    }
-    app.draw(ScoreText);
-    app.draw(Highest_ScoreText);
-    app.display();
 }
+
+int main()
+{
+    srand(time(0));
+    app.setFramerateLimit(60);
+    t1.loadFromFile("images/background2.jpg");
+    t2.loadFromFile("images/platform.png");
+    t3_1.loadFromFile("images/dog2_left.png");
+    t3_2.loadFromFile("images/dog2_right.png");
+    t4.loadFromFile("images/GameOver_fix.png");
+    t5.loadFromFile("images/bonk_img_resize.jpg");
+
+    sBackground.setTexture(t1),
+    sPlat.setTexture(t2),
+    sPers_left.setTexture(t3_1),
+    sPers_right.setTexture(t3_2),
+    sGameOver.setTexture(t4),
+    sBonk.setTexture(t5);
+
+    font.loadFromFile("font/arial.ttf");
+
+    ScoreText.setFont(font);
+    ScoreText.setCharacterSize(20);
+    ScoreText.setFillColor(Color::Red);
+
+
+    Highest_ScoreText.setFont(font);
+    Highest_ScoreText.setCharacterSize(20);
+    Highest_ScoreText.setFillColor(Color::Red);
+    Highest_ScoreText.setPosition (200,0);
+
+    Restart.setFont(font);
+    Restart.setCharacterSize(30);
+    Restart.setFillColor(Color::Black);
+    Restart.setString("Press F to restart!");
+    Restart.setPosition(80,480);
+
+    buffer.loadFromFile("sound/jump.wav");
+
+    jump.setBuffer(buffer);
+
+    buffer_1.loadFromFile("sound/bonk.wav");
+
+    bonk.setBuffer(buffer_1);
+
+    set_start();
+    run();
     return 0;
 }
